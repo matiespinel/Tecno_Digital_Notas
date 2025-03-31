04f571edcde04036a8554cb3fe9b6fdf289520fe
matiespinel <matiasespinel.me@gmail.com>
2025-03-31 12:41:26 -0300

    Update Funcionestp.py

--- Funcionestp.py
+++ Funcionestp.py
@@ -11,4 +11,27 @@ def simBP (a : int, b : int) -> int:
         
     return conta
 print(simBP(4,4))     
-        
+def conjunto_a_k(a : int, b : int, n : int, k : int):
+    i = a
+    while (i <= b):
+        resSIMBP = simBP(i, n)
+        res = 0 
+        if resSIMBP == k:
+            res = res + 1
+        i = i + 1
+    return res
+
+def existe_simBP(a : int, b : int, n : int, k : int):
+    if conjunto_a_k(a, b, n, k) > 0:
+        return True
+    else:
+        return False
+
+def simBP_Menores(a : int, b : int, n : int):
+    tienen = []
+    i = a
+    t = 0
+    while (i <= b):
+        resSIMBP = simBP(i, n)
+        tienen.append(i ,resSIMBP)
+        i = i + 1
