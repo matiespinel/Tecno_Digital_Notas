1554517e4ae6537b73144cbe67005f680ec346ca
Matias Espinel <matiasespinel.me@gmail.com>
2025-10-19 14:45:52 -0300

    Update GuiaDeEjer.C

--- TD2/C/GuiaDeEjer.C
+++ TD2/C/GuiaDeEjer.C
@@ -14,8 +14,20 @@ int len(char* s){
         
 }
 char* copy(char* s){// toma un str y genera una copia del mismo
+    char* ret;
     for (int i = 0; i< len(s);i++){
-        
+        char caracter = s[i];
+        ret[i] = caracter; 
+    }
+    return ret;
+    }
+
+void replaceChar(char* s, char old, char nuevow){
+    for (int i = 0; i< len(s);i++){
+        char caracter = s[i];
+        if (caracter == old){
+            s[i] = nuevow;
+        } 
     }
 }
 
