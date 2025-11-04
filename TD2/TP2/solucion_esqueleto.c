6ad37e9f8ae5128502cb8aad247ec0572d90eb69
Matias Espinel <matiasespinel.me@gmail.com>
2025-11-04 11:05:11 -0300

    Update solucion_esqueleto.c

--- TD2/TP2/solucion_esqueleto.c
+++ TD2/TP2/solucion_esqueleto.c
@@ -3,6 +3,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+// ===========ejericio 1 ==========
+//funciones 
+char* strDuplicate(char* src){
+    int contador = 0; 
+    while (src[contador] != '\0') { // me fijo cuando termina el str 
+        contador++;// longitud del src
+        printf("contador: %d\n", contador);//para ver que este andno bien
+    }
+    char* ret = (char*)malloc(sizeof(char) * (contador + 1)); // +1 para el \0?
+    for(int i =0; i <contador; i++){
+        ret[i] = src[i];
+    }
+    ret[contador] = '\0';
+    return ret;
+}
+//comparacion lexicografica
+int strCompare(char* s1, char* s2) {
+    int i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0') {
+        if (s1[i] < s2[i]) return -1;  // s1 es menor
+        if (s1[i] > s2[i]) return 1;   // s1 es mayor
+        i++;
+    }
+
+    // Si uno terminó antes que el otro
+    if (s1[i] == '\0' && s2[i] == '\0') return 0;  // iguales
+    if (s1[i] == '\0') return -1;  // s1 más corto → menor
+    return 1;                      // s2 más corto → mayor
+}
+
+char* strConcatenate(char* src1, char* src2){
+    int len1 =0;
+    int len2 =0;
+    while (src1[len1] != '\0'){
+        len1++;
+    }
+    while (src2[len2] != '\0'){
+        len2++;
+    }
+    char* ret = (char*) malloc (sizeof(char) * (len1 + len2 +1)); // +1 para el \0?
+    for(int i =0; i < len1; i++){
+        ret[i] = src1[i];
+    }
+    for(int j =0; j < len2; j++){
+        ret[len1 + j] = src2[j];
+    }
+    ret[len1 + len2] = '\0';
+    free(src1);
+    free(src2);
+    return ret;
+}
 
 // ========= CONSTANTES DEL JUEGO =========
 #define SCREEN_WIDTH 900
@@ -525,5 +576,69 @@ int main(int argc, char* args[]) {// sumo game board como parametro porque lo ne
 
     gameBoardDelete(game_board);
     cerrar();
+    char* original = "Hola, Mundo!";
+    printf("Original: %s\n", original);
+    char* copia = strDuplicate(original);
+    printf("Copia: %s\n", copia);
+    free(copia);
+
+    //1. String vac ́ıo.
+    char* vacio = "";
+    char* copiaVacio = strDuplicate(vacio);
+    printf("Original: %s\n", vacio);
+    printf("Copia: %s\n", copiaVacio);
+    free(copiaVacio);
+
+    //2. String de un car ́acter.
+    char* unCaracter = "A";
+    char* copiaUnCaracter = strDuplicate(unCaracter);
+    printf("Original: %s\n", unCaracter);
+    printf("Copia: %s\n", copiaUnCaracter);
+    free(copiaUnCaracter);
+    //3. String que incluya todos los caracteres v ́alidos distintos de cero.
+    char* todosCaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':\",.<>/?`~ ";
+    char* copiaTodosCaracteres = strDuplicate(todosCaracteres);
+    printf("Original: %s\n", todosCaracteres);
+    printf("Copia: %s\n", copiaTodosCaracteres);
+    free(copiaTodosCaracteres);
+
+    //strCompare:
+    //1. Dos string vac ́ıos.
+    char* vacio1 = "";
+    char* vacio2 = "";
+    printf("Comparacion de dos strings vacios: %d\n", strCompare(vacio1, vacio2)); // Esperado: 0
+
+    //2. Dos string de un car ́acter.
+    char* charA = "A";
+    char* charB = "B";
+    printf("Comparacion de 'A' y 'B': %d\n", strCompare(charA, charB)); // Esperado: -1
+    //3. Strings iguales hasta un car ́acter (hacer cmpStr(s1,s2) y cmpStr(s2,s1)).
+    char* s1 = "HelloX";
+    char* s2 = "HelloY";
+    printf("Comparacion de '%s' y '%s': %d\n", s1, s2, strCompare(s1, s2)); // Esperado: -1
+    printf("Comparacion de '%s' y '%s': %d\n", s2, s1, strCompare(s2, s1)); // Esperado: 1
+    //4. Dos strings diferentes (hacer cmpStr(s1,s2) y cmpStr(s2,s1)).
+    char* diff1 = "Sistema Operativo para para teclado";
+    char* diff2 = "Pileta redonda en dias de lluvia";
+    printf("Comparacion de '%s' y '%s': %d\n", diff1, diff2, strCompare(diff1, diff2)); // Esperado: 1
+    printf("Comparacion de '%s' y '%s': %d\n", diff2, diff1, strCompare(diff2, diff1)); // Esperado: -1
+    //strConcatenate:
+    //1. Un string vac ́ıo y un string de 3 caracteres.
+    char* vacioConcat = "";
+    char* tresCaracteres = "abc";
+    char* resultadoConcat1 = strConcatenate(strDuplicate(vacioConcat), strDuplicate(tresCaracteres));//el duplicado ya que la funcion deja libre la memoria
+    printf("Concatenacion de vacio y 'abc': %s\n", resultadoConcat1);
+    //2. Un string de 3 caracteres y un string vac ́ıo.
+    char* resultadoConcat2 = strConcatenate(strDuplicate(tresCaracteres), strDuplicate(vacioConcat));
+    printf("Concatenacion de 'abc' y vacio: %s\n", resultadoConcat2);
+    //3. Dos strings de 1 caracter.
+    char* charX = "X";
+    char* charY = "Y";
+    char* resultadoConcat3 = strConcatenate(strDuplicate(charX), strDuplicate(charY));
+    //4. Dos strings de 5 caracteres.
+    char* cincoX = "ABCDE";
+    char* cincoY = "FGHIJ";
+    char* resultadoConcat4 = strConcatenate(strDuplicate(cincoX), strDuplicate(cincoY));
+    printf("Concatenacion de '%s' y '%s': %s\n", cincoX, cincoY, resultadoConcat4);
     return 0;
 }
